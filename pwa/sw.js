8851e24e6b5a7b10f8b3a37c686a1b79e7cb4697
Samuel Martinez <samuelmartinez@192.168.1.105>
2026-04-05 00:13:44 -0600

    Add all apps with PWA support

---
+++ pwa/sw.js
@@ -0,0 +1,29 @@
+const CACHE='sds-v1';
+const ASSETS=[
+  './monitoring.html',
+  'https://unpkg.com/react@18.2.0/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18.2.0/umd/react-dom.production.min.js',
+  'https://unpkg.com/prop-types@15.8.1/prop-types.min.js',
+  'https://unpkg.com/recharts@2.7.3/umd/Recharts.js',
+  'https://unpkg.com/@babel/standalone@7.24.0/babel.min.js'
+];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch',e=>{
+  e.respondWith(
+    fetch(e.request).then(r=>{
+      const clone=r.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,clone));
+      return r;
+    }).catch(()=>caches.match(e.request))
+  );
+});
